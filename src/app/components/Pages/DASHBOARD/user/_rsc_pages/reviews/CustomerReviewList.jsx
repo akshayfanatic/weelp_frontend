611d2d880d389ce51b84f417b2aacc1aad7c065b
akshayfanatic <akshay@fanaticcoders.com>
2025-09-02 13:59:14 +0530

    testing script to deploy again

--- src/app/components/Pages/DASHBOARD/user/_rsc_pages/reviews/CustomerReviewList.jsx
+++ src/app/components/Pages/DASHBOARD/user/_rsc_pages/reviews/CustomerReviewList.jsx
@@ -1,45 +1,31 @@
 import React from "react";
 import BookingCard from "@/app/components/BookingCard";
-import {
-  Card,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { UserDashboardReviewCard } from "@/app/components/ReviewCard";
 
 export const CustomerReviewList = () => {
   return (
     <Card className="  shadow-none border-none bg-inherit  bg-white">
       <CardHeader className={"px-8"}>
-        <CardTitle className="text-xl text-Blueish font-medium">
-          Your Reviews
-        </CardTitle>
-        <CardDescription className="text-lg text-grayDark">
-          Manage your Reviews, Create New.
-        </CardDescription>
+        <CardTitle className="text-xl text-Blueish font-medium">Your Reviews</CardTitle>
+        <CardDescription className="text-lg text-grayDark">Manage your Reviews, Create New.</CardDescription>
       </CardHeader>
-      
 
       <Tabs defaultValue="active" className="w-full">
         <div className="p-8 pt-0 flex justify-between">
           <TabsList>
-            <TabsTrigger className={"px-6"} value="active">Active</TabsTrigger>
-            <TabsTrigger className={"px-6"} value="draft">Draft</TabsTrigger>
+            <TabsTrigger className={"px-6"} value="active">
+              Active
+            </TabsTrigger>
+            <TabsTrigger className={"px-6"} value="draft">
+              Draft
+            </TabsTrigger>
           </TabsList>
           <Select>
             <SelectTrigger className="w-[120px]">
-              <SelectValue placeholder="Filters"  />
+              <SelectValue placeholder="Filters" />
             </SelectTrigger>
             <SelectContent>
               <SelectGroup>
@@ -54,17 +40,14 @@ export const CustomerReviewList = () => {
         <div className="bg-[#f5f9fa] p-8 min-h-full h-[78vh]">
           <TabsContent value="active">
             {/** Here items come ** */}
-            <div className="flex flex-wrap  bg-[#F5F9FA] gap-4"> 
+            <div className="flex flex-wrap  bg-[#F5F9FA] gap-4">
               <UserDashboardReviewCard />
               <UserDashboardReviewCard />
               <UserDashboardReviewCard />
             </div>
           </TabsContent>
 
-
-          <TabsContent value="draft">
-
-          </TabsContent>
+          <TabsContent value="draft"></TabsContent>
         </div>
       </Tabs>
     </Card>
